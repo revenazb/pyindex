59dca6d3011d88aca4642275ae42d338f668450d
Bertrand Revenaz <revenazb@gmail.com>
2014-06-12 18:00:18 +0100

    Update test.py
    oops

--- pyindex/test.py
+++ pyindex/test.py
@@ -15,10 +15,6 @@ class TestInterleave(unittest.TestCase):
         self.assertEqual(hex(interleave.interleave3(0x00, 0xFF, 0x00)), '0x492492')
         self.assertEqual(hex(interleave.interleave3(0x0000, 0xFFFF, 0x0000)), '0x12492492')
 
-    def test_interleave4(self):
-        self.assertEqual(hex(interleave.interleave4(0xFF, 0x00, 0xFF, 0x00)), '0xaaaaaaaa')
-       self.assertEqual(hex(interleave.interleave4(0xFFFF, 0x0000, 0xFFFF, 0x0000)).rstrip('L'), '0xaaaaaaaaaaaaaaaa')
-
     def test_idempotency(self):
         integers = (4, 42)
         interleaved = interleave.interleave2(*integers)
